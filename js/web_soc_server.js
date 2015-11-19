5e0127cfd603a73a451dc3064b7598e40480c7c6
Kairm Mansour <senegalo@gmail.com>
2015-11-19 18:19:40 +0200

    finalized the relay server

--- js/web_soc_server.js
+++ js/web_soc_server.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 Server = {
+    connections: {},
+    connectionsID: 0,
     init: function() {
+        var self = this;
+        
         var WebSocketServer = require('websocket').server;
         var http = require('http');
         var server = http.createServer(function(request, response) {
@@ -25,120 +29,44 @@ Server = {
                 Server.logEvent('Connection from origin ' + request.origin + ' rejected.');
                 return;
             }
-
-            var connection = request.accept('test-protocol', request.origin);
-            connection.on('message', function(message) {
-                if (message.type === 'utf8') {
-                    Server.logEvent("Recived Message...");
-                    var parsedMsg = JSON.parse(message.utf8Data);
-                    if (Commands[parsedMsg.type + "Command"]) {
-                        Server.logEvent("Recived Command...");
-                        parsedMsg.connection = connection;
-                        Commands[parsedMsg.type + "Command"](parsedMsg);
-                    }
-                }
-            });
-            connection.on('close', function(reasonCode, description) {
-                Server.logEvent('Peer ' + connection.remoteAddress + ' disconnected.');
-                Commands.closeConnection(connection.remoteAddress);
-            });
+            
+            self.acceptConnection(request);
         });
     },
-    originIsAllowed: function(origin) {
-        // put logic here to detect whether the specified origin is allowed.
-        return true;
-    },
-    sendObject: function(connection, obj) {
-        var str = JSON.stringify(obj);
-        Server.logEvent("sending data...");
-        connection.send(str);
-    },
-    logEvent: function(msg){
-        console.log((new Date()) + msg);
-    }
-};
-
-Commands = {
-    extend: require('util')._extend,
-    players: {},
-    initCommand: function(cmd) {
-        this.players[cmd.id] = {
-            connection: cmd.connection,
-            engaged: false,
-            id: cmd.id
+    
+    accectConnection: function (request) {
+        var self = this;
+        var guid = this.getGUID();
+        var connection = request.accept('test-protocol', request.origin);
+        
+        this.connections[guid] = {
+            connection: connection,
+            pairedWith: false
         };
-        var player = this.players[cmd.id];
-        for (var p in this.players) {
-            var op = this.players[p];
-            if (p === cmd.id) {
-                continue;
-            }
-            if (!op.engaged) {
-                op.engaged = true;
-                op.playingWith = cmd.id;
-                player.engaged = true;
-                player.playingWith = p;
-                Server.logEvent("Connecting "+player.id+" With "+op.id);
-                break;
-            }
-        }
-    },
 
-    forwardCommand: function(cmd){
-      var player = this.players[cmd.id];
-      var op = this.players[player.playingWith];
-      Server.sendObject(op.connection, { type: "forward", state: cmd.state});
-    },
+        connection.send(guid);
 
-    actionCommand: function(cmd) {
-        var player = this.players[cmd.connection.remoteAddress];
-        var op = this.players[player.playingWith];
-        if(op.state[cmd.target]){
-            op.state[cmd.target].hp += cmd.impact;
-            Server.logEvent("Unit ID: "+cmd.target+" For User: " + op.connection.remoteAddress + " Current Health: "+op.state[cmd.target].hp);
-            this.syncActions(player,op,"sync");
-        } else {
-            player.connection.send("NO TROUPS FOUND WITH ID "+cmd.target);
-        }
-    },
-    syncActions: function(player, op, type) {
-        Server.logEvent("Syncying Sessions...");
-        Server.sendObject(player.connection, {
-            type: type,
-            yourState: this.convertStateToArray(player.state),
-            opState: this.convertStateToArray(op.state)
+        connection.on('message', function (message) {
+            var conObj = self.connections[guid];
+            if(conObj.pairedWith === false){
+                self.connections[guid].pairedWith = message.utf8Data;
+                self.connections[message.utf8Data].pairedWith = guid;
+            } else {
+                self.connections[conObj.pariedWith].connection.send(message.utf8Data);
+            }
         });
-        Server.sendObject(op.connection, {
-            type: type,
-            yourState: this.convertStateToArray(op.state),
-            opState: this.convertStateToArray(player.state)
+
+        connection.on('close', function (reasonCode, description) {
+            var pairID = self.connections[guid].pairedWith;
+            self.connections[pairID] && self.connections[pairID].connection.close();
+            Server.logEvent('Peer ' + connection.remoteAddress + ' disconnected.');
+            delete self.connections[guid];
+            delete self.connections[pairID];
         });
     },
-    convertStateToArray: function(state){
-        var out = [];
-        for(var s in state){
-            out.push(state[s]);
-        }
-        return out;
-    },
-    convertStateToObject: function(state){
-        var out = {};
-        for(var s in state){
-            out[state[s].id] = state[s];
-        }
-        return out;
-    },
-    closeConnection: function(id){
-        var player = this.players[id];
-        if(player.engaged){
-            var op = this.players[player.playingWith];
-            Server.sendObject(op.connection,{
-                type: "connectionClosed"
-            });
-            op.engaged = false;
-            op.state = this.extend({},op.initialState);
-        }
-        delete this.players[id];
+    
+    getGUID: function(){
+        return this.connectionsID++;
     }
 };
 
